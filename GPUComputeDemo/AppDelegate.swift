38bff363035253ba0916122fcc60c9ce276ab90a
liuruilong <liuruilong@baidu.com>
2019-07-11 16:52:08 +0800

    initial commit

---
+++ GPUComputeDemo/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  GPUComputeDemo
+//
+//  Created by liuRuiLong on 2019/7/11.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ GPUComputeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPUComputeDemo
+//
+//  Created by liuRuiLong on 2019/7/11.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
